eb1ac7f9f776dfdc25ce9b3db37838a739c85ef8
Khoirul Anam <khoirulanam592000@gmail.com>
2025-12-01 18:40:57 +0700

    fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/inventori_ban';
+$config['base_url'] = 'http://localhost/stockban';
 
 /*
 |--------------------------------------------------------------------------
